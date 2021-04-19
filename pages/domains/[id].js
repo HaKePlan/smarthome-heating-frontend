734b235f3ea6380f387fa5de404a0058edbdb0d4
HaKePlan <severin.clauss@gmail.com>
2021-04-19 03:09:29 +0200

    added dashboard and fixed domains site

--- pages/domains/[id].js
+++ pages/domains/[id].js
@@ -1,33 +1,15 @@
 import { useRouter } from 'next/router';
 import Head from 'next/head';
 import useSWR from 'swr';
-import Banner from '../../components/banner2';
 import Layout from '../../components/layout';
 import fetcher from '../../lib/fetcher';
+import swrFetch from '../../lib/fetcher';
 
 export default function Domain() {
   const router = useRouter();
   const { id } = router.query;
 
-  let { data, error } = useSWR(
-    `http://127.0.0.1:3000/api/v1/modbus/domain/${id}`,
-    fetcher,
-    {
-      revalidateOnFocus: true,
-      refreshInterval: 5000, // 5s
-    }
-  );
-
-  let element = 'Loading...';
-
-  if (!data && error) {
-    // element = `${error.statusCode} ${error.message}`;
-    element = (
-      <div>
-        <a>{error.statusCode} nothing to see here</a>
-      </div>
-    );
-  }
+  let { data, error } = swrFetch(`domain/${id}`);
 
   if (data) {
     data.data.data.map((doc) => {
@@ -47,22 +29,23 @@ export default function Domain() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <body>
-        <Banner />
-        <div>
-          {data ? (
-            <ul>
-              {data.data.data.map(({ name, value, unit, _id }) => (
-                <li class="listItem" key={_id}>
-                  <a>{name}: </a>
-                  <a>
-                    {value} {unit}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          ) : (
-            element
-          )}
+        <div class="absolute h-3/4 w-screen">
+          <div class="text-black absolute left-64 top-64">
+            {data ? (
+              <ul>
+                {data.data.data.map(({ name, value, unit, _id }) => (
+                  <li class="listItem" key={_id}>
+                    <a>{name}: </a>
+                    <a>
+                      {value} {unit}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              'Loading...'
+            )}
+          </div>
         </div>
       </body>
     </Layout>
--- pages/utilities/dashboard.js
+++ pages/utilities/dashboard.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Link from 'next/link';
 import Layout from '../../components/layout';
 
 export default function Alarm() {
@@ -7,7 +8,52 @@ export default function Alarm() {
       <Head>
         <title>heating client | dashboard</title>
       </Head>
-      <div>nothing to see here, only the dashboard</div>
+      <div class="h-screen w-screen relative">
+        <div class="h-min w-screen flex flex-row flex-wrap absolute top-24">
+          <div class="h-72 w-1/3 flex justify-center items-center">
+            <Link href="/domains/heizgruppe_1">
+              <div class="cursor-pointer w-48 h-48 bg-red-50 flex justify-center items-center">
+                <p class="">heizgruppe 1</p>
+              </div>
+            </Link>
+          </div>
+          <div class="h-72 w-1/3 flex justify-center items-center">
+            <Link href="/domains/heizgruppe_2">
+              <div class="cursor-pointer w-48 h-48 bg-red-50 flex justify-center items-center">
+                <p class="">heizgruppe 2</p>
+              </div>
+            </Link>
+          </div>
+          <div class="h-72 w-1/3 flex justify-center items-center">
+            <Link href="/domains/vorregler">
+              <div class="cursor-pointer w-48 h-48 bg-red-50 flex justify-center items-center">
+                <p class="">vorregler</p>
+              </div>
+            </Link>
+          </div>
+          <div class="h-72 w-1/3 flex justify-center items-center">
+            <Link href="/domains/allgemein">
+              <div class="cursor-pointer w-48 h-48 bg-red-50 flex justify-center items-center">
+                <p class="">allgemein</p>
+              </div>
+            </Link>
+          </div>
+          <div class="h-72 w-1/3 flex justify-center items-center">
+            <Link href="/domains/brauchwasser">
+              <div class="cursor-pointer w-48 h-48 bg-red-50 flex justify-center items-center">
+                <p class="">brauchwasser</p>
+              </div>
+            </Link>
+          </div>
+          <div class="h-72 w-1/3 flex justify-center items-center">
+            <Link href="/domains/alarme">
+              <div class="cursor-pointer w-48 h-48 bg-red-50 flex justify-center items-center">
+                <p class="">alarme</p>
+              </div>
+            </Link>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 }
