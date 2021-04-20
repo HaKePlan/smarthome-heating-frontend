96a7295f78ff30232c521f673ea63770e8dc03d0
HaKePlan <severin.clauss@gmail.com>
2021-04-20 19:44:15 +0200

    dev

--- pages/domains/[id].js
+++ pages/domains/[id].js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 import useSWR from 'swr';
+import React, { useState } from 'react';
 
 import Layout from '../../components/layout';
 import fetcher from '../../lib/fetcher';
@@ -9,6 +10,7 @@ import swrFetch from '../../lib/fetcher';
 export default function Domain() {
   const router = useRouter();
   const { id } = router.query;
+  const [showMe, setShowMe] = useState(false);
 
   let { data, error } = swrFetch(`domain/${id}`);
 
@@ -19,36 +21,98 @@ export default function Domain() {
         const val = doc.value;
         doc.value = doc.valueAssignation.assignment[val];
       }
-      return doc;
     });
   }
 
+  const container = () => {
+    const divArr = [];
+
+    let i = 1;
+    data.data.data.forEach((element) => {
+      divArr.push(
+        <div>
+          <div
+            className="flex flex-row relative space-x-36 p-6 hover:bg-gray-50 hover:shadow-md rounded-md border border-transparent hover:border-gray-200"
+            onClick={(value) => {
+              toggle();
+              setID(value);
+            }}
+            value={element._id}
+          >
+            <div className="flex-grow">{element.name}</div>
+            <div className="flex felx-row space-x-1 relative">
+              <div>{element.value}</div>
+              <div>{element.unit}</div>
+            </div>
+          </div>
+          {i === data.data.data.length ? (
+            ''
+          ) : (
+            <div className="w-auto border-b-2 border-red-200 mx-6" />
+          )}
+        </div>
+      );
+      i++;
+    });
+
+    return divArr.map((el) => {
+      return el;
+    });
+  };
+
+  function setID(el) {
+    console.log(el);
+    console.log(el.target);
+  }
+
+  function toggle() {
+    setShowMe(!showMe);
+  }
+
+  function submit() {
+    console.log('monog');
+    setShowMe(!showMe);
+  }
   return (
     <Layout>
       <Head>
         <title>heating client | {id}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <body>
-        <div className="absolute h-3/4 w-screen">
-          <div className="text-black absolute left-64 top-64">
-            {data ? (
-              <ul>
-                {data.data.data.map(({ name, value, unit, _id }) => (
-                  <li className="listItem" key={_id}>
-                    <a>{name}: </a>
-                    <a>
-                      {value} {unit}
-                    </a>
-                  </li>
-                ))}
-              </ul>
-            ) : (
-              'Loading...'
-            )}
+      <div className="relative w-screen h-screen flex justify-center">
+        <div className="flex flex-col justify-center absolute mt-24 border-2 border-red-200 rounded-md">
+          {!data ? (
+            <div className="w-60 h-60 flex justify-center items-center">
+              <p>loading...</p>
+            </div>
+          ) : (
+            container()
+          )}
+        </div>
+        <div
+          className="absolute h-72 w-72 bg-gray-100 bg-opacity-70"
+          style={{
+            display: showMe ? 'block' : 'none',
+          }}
+        >
+          <div className="absolute bottom-0">
+            <button
+              className="font-bold text-red-700 bg-red-100 rounded-md h-10 w-20 hover:bg-red-300"
+              onClick={toggle}
+            >
+              cancle
+            </button>
+            <button
+              className="font-bold text-green-700 bg-green-100 rounded-md h-10 w-20 hover:bg-green-300"
+              type="submit"
+              value="submit"
+              onClick={submit}
+            >
+              submit
+            </button>
           </div>
         </div>
-      </body>
+      </div>
     </Layout>
   );
 }
