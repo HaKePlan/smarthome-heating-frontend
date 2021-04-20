afa87260439d36d5a76dfaf4f8550ae33c6143ae
HaKePlan <severin.clauss@gmail.com>
2021-04-20 22:20:52 +0200

    dev set value for entry window

--- pages/domains/[id].js
+++ pages/domains/[id].js
@@ -11,6 +11,7 @@ export default function Domain() {
   const router = useRouter();
   const { id } = router.query;
   const [showMe, setShowMe] = useState(false);
+  const [entry, setEntry] = useState('');
 
   let { data, error } = swrFetch(`domain/${id}`);
 
@@ -33,11 +34,17 @@ export default function Domain() {
         <div>
           <div
             className="flex flex-row relative space-x-36 p-6 hover:bg-gray-50 hover:shadow-md rounded-md border border-transparent hover:border-gray-200"
-            onClick={(value) => {
+            onClick={() => {
               toggle();
-              setID(value);
+              setEntry(
+                <div className="flex flex-row relative space-x-36 p-6 font-semibold">
+                  <div className="flex-grow">{element.name}</div>
+                  <div className="flex felx-row space-x-1 relative">
+                    <div>{element.value}</div>
+                  </div>
+                </div>
+              );
             }}
-            value={element._id}
           >
             <div className="flex-grow">{element.name}</div>
             <div className="flex felx-row space-x-1 relative">
@@ -60,11 +67,6 @@ export default function Domain() {
     });
   };
 
-  function setID(el) {
-    console.log(el);
-    console.log(el.target);
-  }
-
   function toggle() {
     setShowMe(!showMe);
   }
@@ -80,36 +82,40 @@ export default function Domain() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="relative w-screen h-screen flex justify-center">
-        <div className="flex flex-col justify-center absolute mt-24 border-2 border-red-200 rounded-md">
-          {!data ? (
-            <div className="w-60 h-60 flex justify-center items-center">
-              <p>loading...</p>
+        <div className="flex w-screen absolute mt-24 justify-center items-center">
+          <div className="flex flex-col justify-center border-2 border-red-200 rounded-md">
+            {!data ? (
+              <div className="w-60 h-60 flex justify-center items-center">
+                <p>loading...</p>
+              </div>
+            ) : (
+              container()
+            )}
+          </div>
+          <div
+            className="absolute border-2 border-red-500 rounded-md p-8 flex-col flex space-y-5 bg-gray-50 shadow-2xl"
+            style={{
+              display: showMe ? 'block' : 'none',
+            }}
+          >
+            {entry}
+            <div className="w-auto border-b-2 border-red-500" />
+            <div className="bottom-0 flex flex-row justify-center space-x-10">
+              <button
+                className="font-bold text-red-700 bg-red-100 rounded-md h-10 w-20 hover:bg-red-300 flex-grow"
+                onClick={toggle}
+              >
+                cancle
+              </button>
+              <button
+                className="font-bold text-green-700 bg-green-100 rounded-md h-10 w-20 hover:bg-green-300 flex-grow"
+                type="submit"
+                value="submit"
+                onClick={submit}
+              >
+                submit
+              </button>
             </div>
-          ) : (
-            container()
-          )}
-        </div>
-        <div
-          className="absolute h-72 w-72 bg-gray-100 bg-opacity-70"
-          style={{
-            display: showMe ? 'block' : 'none',
-          }}
-        >
-          <div className="absolute bottom-0">
-            <button
-              className="font-bold text-red-700 bg-red-100 rounded-md h-10 w-20 hover:bg-red-300"
-              onClick={toggle}
-            >
-              cancle
-            </button>
-            <button
-              className="font-bold text-green-700 bg-green-100 rounded-md h-10 w-20 hover:bg-green-300"
-              type="submit"
-              value="submit"
-              onClick={submit}
-            >
-              submit
-            </button>
           </div>
         </div>
       </div>
