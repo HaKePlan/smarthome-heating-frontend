79772800b60a40eac6a873c266de1423d0691d5d
HaKePlan <severin.clauss@gmail.com>
2021-05-15 11:41:00 +0200

    solving data fetching problem

--- pages/entrys/[id].js
+++ pages/entrys/[id].js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 import cookie from 'js-cookie';
 import { useRouter } from 'next/router';
-import Router from 'next/router';
+// import Router from 'next/router';
 import React, { useState } from 'react';
-import useSWR from 'swr';
 
+// import useSWR from 'swr';
 import Layout from '../../components/layout';
 import AppError from '../../lib/appError';
 // import patchFetcher from '../../lib/patchFetcher';
@@ -13,6 +13,7 @@ const fetcher = (url) =>
   axios
     .get(url)
     .then((res) => {
+      console.log('its solved');
       return res;
     })
     .catch((err) => {
@@ -67,50 +68,46 @@ const postEntry = (id) => {
     // patchFetcher(data.data.data._id, val, token);
   }
 
-  if (!data || data.status === 'pending') {
+  if (data) {
+    console.log(data);
+    return (
+      <div className="absolute border-2 border-red-500 rounded-md p-8  bg-gray-50 shadow-2xl flex-col flex space-y-5 bg-gray-50">
+        <div className="flex flex-row relative space-x-36 p-6 font-semibold">
+          <div className="flex-grow">{data.data.data.name}</div>
+          <div className="flex felx-row space-x-1 relative">
+            <select
+              value={value}
+              onChange={(e) => {
+                setValue(e.target.value);
+              }}
+            >
+              {options(data.data.data)}
+            </select>
+          </div>
+        </div>
+        <div className="w-auto border-b-2 border-red-500" />
+        <div className="bottom-0 flex flex-row justify-center space-x-10">
+          <button className="font-bold text-red-700 bg-red-100 rounded-md h-10 w-20 hover:bg-red-300 flex-grow">
+            cancle
+          </button>
+          <button
+            className="font-bold text-green-700 bg-green-100 rounded-md h-10 w-20 hover:bg-green-300 flex-grow"
+            type="submit"
+            onClick={() => {
+              submit(value);
+            }}
+          >
+            submit
+          </button>
+        </div>
+      </div>
+    );
+  } else {
+    console.log('loading');
     return <p>loading...</p>;
   }
 
-  console.log(data);
-
   // const value = data.data.data.value;
-  return (
-    <div className="absolute border-2 border-red-500 rounded-md p-8  bg-gray-50 shadow-2xl flex-col flex space-y-5 bg-gray-50">
-      <div className="flex flex-row relative space-x-36 p-6 font-semibold">
-        <div className="flex-grow">{data.data.data.name}</div>
-        <div className="flex felx-row space-x-1 relative">
-          <select
-            value={value}
-            onChange={(e) => {
-              setValue(e.target.value);
-            }}
-          >
-            {options(data.data.data)}
-          </select>
-        </div>
-      </div>
-      <div className="w-auto border-b-2 border-red-500" />
-      <div className="bottom-0 flex flex-row justify-center space-x-10">
-        <button
-          className="font-bold text-red-700 bg-red-100 rounded-md h-10 w-20 hover:bg-red-300 flex-grow"
-          onClick={() => {
-            Router.push(`../domains/${from}`);
-          }}
-        >
-          cancle
-        </button>
-        <button
-          className="font-bold text-green-700 bg-green-100 rounded-md h-10 w-20 hover:bg-green-300 flex-grow"
-          type="submit"
-          onClick={() => {
-            submit(value);
-          }}
-        >
-          submit
-        </button>
-      </div>
-    </div>
-  );
 };
 
 export default function Entry() {
