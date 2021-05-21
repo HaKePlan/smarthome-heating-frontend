3bc753a23b6de213948a4d8f9472f4bbbf1730de
HaKePlan <severin.clauss@gmail.com>
2021-05-21 23:42:12 +0200

    ui updated

--- pages/index.js
+++ pages/index.js
@@ -1,54 +1,88 @@
 import Head from 'next/head';
+import Router from 'next/router';
 
 import Layout from '../components/layout';
 import swrFetch from '../lib/fetcher';
 
-function Home() {
-  // get token
-  // const token = cookie.get('token');
-  // console.log(token);
-
+export default function Home() {
+  // 1) GET DATA FROM API SERVER
   let { data } = swrFetch('home');
 
+  // 2) MAINIPULATE DATA TO SHOW CLEAN
   if (data) {
-    data.data.data.map((doc) => {
+    data.data.data.forEach((doc) => {
       if (doc.unit === 'signedValue') {
-        doc.unit = '';
+        doc.showUnit = '';
         const val = doc.value;
-        doc.value = doc.valueAssignation.assignment[val];
+        doc.showValue = doc.valueAssignation.assignment[val];
+      } else {
+        doc.showValue = doc.value;
+        doc.showUnit = doc.unit;
       }
-      return doc;
     });
   }
 
+  // 3) SETUP CONTAINER (ALL ENTRYS)
+  const container = () => {
+    const divArr = [];
+    let i = 1;
+
+    // create for each entry a sub container and push it to an array
+    data.data.data.forEach((element) => {
+      divArr.push(
+        <div>
+          <div
+            className="flex flex-row relative space-x-36 p-6 hover:bg-gray-50 hover:shadow-md rounded-md border border-transparent hover:border-gray-200 cursor-pointer"
+            onClick={() => {
+              if (element.register === 4 || element.register === 0) {
+                Router.push(
+                  `../entrys/${element._id}?source=${element.domain}`
+                );
+              }
+            }}
+          >
+            <div className="flex-grow">{element.name}</div>
+            <div className="flex felx-row space-x-1 relative">
+              <div>{element.showValue}</div>
+              <div>{element.showUnit}</div>
+            </div>
+          </div>
+          {i === data.data.data.length ? (
+            ''
+          ) : (
+            <div className="w-auto border-b-2 border-red-200 mx-6" />
+          )}
+        </div>
+      );
+      i += 1;
+    });
+
+    // return each sub container in array
+    return divArr.map((el) => {
+      return el;
+    });
+  };
+
+  // 4) RENDER SITE
   return (
     <Layout>
       <Head>
         <title>heating client | home</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <body>
-        <div className="absolute h-3/4 w-screen">
-          <div className="text-black absolute left-64 top-64">
+      <div className="relative w-screen h-screen flex justify-center">
+        <div className="flex w-screen absolute mt-24 justify-center items-center">
+          <div className="flex flex-col justify-center border-2 border-red-200 rounded-md">
             {data ? (
-              <ul>
-                {data.data.data.map(({ name, value, unit, _id }) => (
-                  <li className="listItem" key={_id}>
-                    <a>{name}: </a>
-                    <a>
-                      {value} {unit}
-                    </a>
-                  </li>
-                ))}
-              </ul>
+              container()
             ) : (
-              'Loading...'
+              <div className="w-60 h-60 flex justify-center items-center">
+                <p>loading...</p>
+              </div>
             )}
           </div>
         </div>
-      </body>
+      </div>
     </Layout>
   );
 }
-
-export default Home;
