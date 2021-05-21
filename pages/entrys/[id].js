35835378a6ba5f52c004e193a7f99ff37c473bd5
HaKePlan <severin.clauss@gmail.com>
2021-05-21 10:40:02 +0200

    fixed entry site

--- pages/entrys/[id].js
+++ pages/entrys/[id].js
@@ -1,12 +1,13 @@
 import axios from 'axios';
 import cookie from 'js-cookie';
+import Link from 'next/link';
 import React, { useState } from 'react';
 
 import Layout from '../../components/layout';
 
 async function submit(val, id) {
   const token = cookie.get('token');
-  const res = await axios
+  await axios
     .patch(
       `http://${process.env.NEXT_PUBLIC_APIHOST}/id/${id}`,
       {
@@ -18,20 +19,11 @@ async function submit(val, id) {
         },
       }
     )
-    .then((r) => {
-      return r;
-    })
     .catch((err) => {
-      return err;
+      console.log(
+        `ERROR: ${err.response.status}, ${err.response.data.message}`
+      );
     });
-
-  if (res.data) {
-    console.log('success');
-    return 'success';
-  } else {
-    console.log(`ERROR: ${res.response.status}, ${res.response.data.message}`);
-    return res.response.data.message;
-  }
 }
 
 const options = (e) => {
@@ -50,44 +42,70 @@ const options = (e) => {
   });
 };
 
-function Entry({ ent }) {
-  const [showMe, setShowMe] = useState('');
-
-  let message = '';
+function Entry({ ent, from }) {
+  const [value, setValue] = useState(ent.value);
+  const token = cookie.get('token');
 
   const postEntry = () => {
-    const [value, setValue] = useState(ent.value);
-
     return (
-      <div className="absolute border-2 border-red-500 rounded-md p-8  bg-gray-50 shadow-2xl flex-col flex space-y-5 bg-gray-50">
-        <div className="flex flex-row relative space-x-36 p-6 font-semibold">
-          <div className="flex-grow">{ent.name}</div>
-          <div className="flex felx-row space-x-1 relative">
-            <select
-              value={value}
-              onChange={(e) => {
-                setValue(e.target.value);
+      <div className="absolute border-2 border-red-500 rounded-md  bg-gray-50 shadow-2xl bg-gray-50">
+        <div className="absolute p-2">
+          <Link href={`/domains/${from}`}>
+            <div className="cursor-pointer">
+              <img src="/arrow_back.svg" />
+            </div>
+          </Link>
+        </div>
+        <div className="flex-col flex space-y-5 p-8">
+          <div className="flex flex-row relative space-x-36 p-6 font-semibold">
+            <div className="flex-grow">{ent.name}</div>
+            <div className="flex felx-row space-x-1 relative">
+              <select
+                value={value}
+                onChange={(e) => {
+                  setValue(e.target.value);
+                }}
+              >
+                {options(ent)}
+              </select>
+            </div>
+          </div>
+          <div className="w-auto border-b-2 border-red-500" />
+          <div className="bottom-0 flex flex-row justify-center space-x-10">
+            <Link href={`/domains/${from}`}>
+              <button className="font-bold text-red-700 bg-red-100 rounded-md h-10 w-20 hover:bg-red-300 flex-grow">
+                cancle
+              </button>
+            </Link>
+            <button
+              className="font-bold text-green-700 bg-green-100 rounded-md h-10 w-20 hover:bg-green-300 flex-grow"
+              type="submit"
+              onClick={() => {
+                submit(value, ent._id);
               }}
             >
-              {options(ent)}
-            </select>
+              submit
+            </button>
           </div>
         </div>
+      </div>
+    );
+  };
+
+  const permDenied = () => {
+    return (
+      <div className="absolute border-2 border-red-500 rounded-md p-8  bg-gray-50 shadow-2xl w-96 h-72 flex-col flex space-y-5 bg-gray-50">
+        <div className="flex flex-row relative space-x-36 p-6 font-semibold">
+          <p className="text-center">
+            You do not have permission to preform this action
+          </p>
+        </div>
         <div className="w-auto border-b-2 border-red-500" />
-        <div className="bottom-0 flex flex-row justify-center space-x-10">
-          <button className="font-bold text-red-700 bg-red-100 rounded-md h-10 w-20 hover:bg-red-300 flex-grow">
+        <Link href={`/domains/${from}`}>
+          <button className="font-bold text-red-700 bg-red-100 rounded-md h-10 hover:bg-red-300 flex-grow">
             cancle
           </button>
-          <button
-            className="font-bold text-green-700 bg-green-100 rounded-md h-10 w-20 hover:bg-green-300 flex-grow"
-            type="submit"
-            onClick={() => {
-              message = submit(value, ent._id);
-            }}
-          >
-            submit
-          </button>
-        </div>
+        </Link>
       </div>
     );
   };
@@ -95,32 +113,22 @@ function Entry({ ent }) {
   return (
     <Layout>
       <div className="relative w-screen h-screen flex justify-center">
-        <div className="absolute w-screen flex justify-center">
-          <div className="mt-8 flex justify-center">
-            <div className="w-96 relative" id="errorBanner">
-              <button className="absolute -right-2 -top-2">
-                <img src="/closeButton.svg" />
-              </button>
-              <p className="font-light text-base w-full bg-red-200 rounded-md p-2 text-red-800">
-                {message}
-              </p>
-            </div>
-          </div>
+        <div className="flex w-screen mt-24 justify-center">
+          {token ? postEntry() : permDenied()}
         </div>
-        <div className="flex w-screen mt-24 justify-center">{postEntry()}</div>
       </div>
     </Layout>
   );
 }
 
-export async function getServerSideProps({ params }) {
+export async function getServerSideProps({ params, query }) {
   const res = await fetch(
     `http://${process.env.NEXT_PUBLIC_APIHOST}/id/${params.id}`
   );
   const ent = await res.json();
 
   // pass data to the page via props
-  return { props: { ent: ent.data } };
+  return { props: { ent: ent.data, from: query.source } };
 }
 
 export default Entry;
